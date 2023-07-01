76c4009d64e837f50236e4a2794e2edfeb028ec4
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-07-01 15:38:57 +0300

    chore: mega link updated

--- src/pages/About/GetCV.jsx
+++ src/pages/About/GetCV.jsx
@@ -5,6 +5,6 @@ export default function GetCV() {
   return <Paper>
     <h1>Looking for More Info?</h1>
     <p className="mb-4">If you are curious about my curriculum vitae, you can inspect it to learn more about me!</p>
-    <a target="_blank" rel="noreferrer" className="button w-fit mt-auto" href="https://mega.nz/file/yj4DlCbR#3DKOSSZB_N-niJ7vymsQ254WpPQ_P9rHG2cThTqtCJ4"><File /> Go to CV</a>
+    <a target="_blank" rel="noreferrer" className="button w-fit mt-auto" href="https://mega.nz/file/fvRVRQyA#g2Zwla24rr_kq6SR6M-EZQRWageSC52hPSgjz1TRUZI"><File /> Go to CV</a>
   </Paper>
 }
