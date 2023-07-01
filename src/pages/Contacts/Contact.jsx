2312f6f0f08fc9b8c2522ebf1679ebf6c4dae2b7
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-07-01 03:28:21 +0300

    style: contacts

--- src/pages/Contacts/Contact.jsx
+++ src/pages/Contacts/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact({ url, color, icon, children }) {
   return <a
     href={url} target="_blank" rel="noreferrer"
-    className="p-2 w-full max-w-[50%] min-w-[50%] rounded-lg flex max-sm:flex-col max-sm:w-fit max-sm:flex-1 gap-4 text-xl font-medium items-center hover:brightness-125 transition-all !no-underline"
+    className="p-2 w-fit max-w-[50%] max-md:min-w-[50%] rounded-lg flex max-md:flex-col max-md:w-fit flex-1 gap-4 text-xl font-medium items-center hover:brightness-125 transition-all !no-underline"
   >
     <div
       className={`p-4 rounded-full text-neutral-200 border-2 border-neutral-800 dark:border-neutral-200`}
