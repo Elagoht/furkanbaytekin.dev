dd13b61b579e16d0feea18daa8a082948e6c6a15
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-12 19:18:39 +0300

    feat: metadata for improved SEO and social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Aside from "@/components/layout/Aside"
 import "@/design/globals.css"
 import classNames from "classnames"
+import { Metadata } from "next"
 import { DM_Sans as DMSans } from "next/font/google"
 import { FC } from "react"
 
@@ -27,5 +28,89 @@ const font = DMSans({
   weight: ["400", "500", "600"],
   subsets: ["latin"]
 })
+
+export const metadata: Metadata = {
+  title: "Furkan Baytekin",
+  description: "Furkan Baytekin's corner on the web. A place to share my thoughts and experiences, If you care.",
+  abstract: "Furkan Baytekin's web book",
+  keywords: ["furkan", "baytekin", "web", "book", "blog", "portfolio", "developer", "programmer", "programming", "musician", "music", "bass guitarist", "music recommendation", "frontend", "backend", "fullstack", "software", "web development"],
+  alternates: {
+    canonical: "https://furkanbaytekin.dev",
+    languages: {
+      "en": "https://furkanbaytekin.dev",
+    }
+  },
+  applicationName: "Furkan Baytekin",
+  authors: [{
+    name: "Furkan Baytekin",
+    url: "https://furkanbaytekin.dev"
+  }],
+  category: "personal blog",
+  creator: "Furkan Baytekin",
+  generator: "Furkan Baytekin",
+  icons: [{
+    url: "/favicon.ico",
+    sizes: "32x32",
+    type: "image/x-icon"
+  }, {
+    url: "/favicon-16x16.png",
+    sizes: "16x16",
+    type: "image/png"
+  }, {
+    url: "/favicon-32x32.png",
+    sizes: "32x32",
+    type: "image/png"
+  }, {
+    url: "/apple-touch-icon.png",
+    sizes: "180x180",
+    type: "image/png"
+  }, {
+    url: "/android-chrome-192x192.png",
+    sizes: "192x192",
+    type: "image/png"
+  },
+  {
+    url: "/android-chrome-512x512.png",
+    sizes: "512x512",
+    type: "image/png"
+  }],
+  manifest: "/site.webmanifest",
+  metadataBase: new URL("https://furkanbaytekin.dev"),
+  publisher: "Furkan Baytekin",
+  referrer: "origin",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  },
+  twitter: {
+    card: "summary",
+    creator: "@furkanbytekin",
+    site: "@furkanbytekin",
+    title: "Furkan Baytekin",
+    creatorId: "furkanbytekin",
+    siteId: "furkanbytekin",
+    description: "Furkan Baytekin's corner on the web. A place to share my thoughts and experiences, If you care."
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://furkanbaytekin.dev",
+    title: "Furkan Baytekin",
+    description: "Furkan Baytekin's corner on the web. A place to share my thoughts and experiences, If you care.",
+    images: [{
+      url: "https://furkanbaytekin.dev/og-image.png",
+      width: 1200,
+      height: 630,
+      alt: "Furkan Baytekin's corner on the web. A place to share my thoughts and experiences, If you care."
+    }],
+    siteName: "Furkan Baytekin",
+    countryName: "Türkiye",
+    emails: ["furkanbaytekin@gmail.com"]
+  }
+}
 
 export default RootLayout
