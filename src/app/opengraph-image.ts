be6279c0226c2bdca73bc960d112cd7598f74e13
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-15 05:01:57 +0300

    feat: OpenGraph and Twitter image generation utilities

---
+++ src/app/opengraph-image.ts
@@ -0,0 +1,14 @@
+import Dictate from "@/utilities/Dictionary"
+import Studio from "@/utilities/Studio"
+
+const dictionary = Dictate.en
+
+export const alt = dictionary.metadata.abstract
+export const type = "image/png"
+export const size = {
+  width: 1200,
+  height: 630
+}
+
+const generator = () => Studio.coverImage(dictionary)
+export default generator
