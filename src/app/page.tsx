d4d7d180878718d0bf5a1371281fc7564e98f3b9
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-15 13:13:46 +0300

    feat: BlogSuggestionCard component and integrate it into the main page with blog suggestions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Content from "@/components/layout/Content"
+import BlogSuggestionCard from "@/components/pages/main/BlogSuggestionCard"
 import BluePrint from "@/utilities/BluePrint"
 import Dictate from "@/utilities/Dictionary"
 import Drawer from "@/utilities/Drawer"
@@ -7,7 +8,7 @@ import { FC } from "react"
 const MainPage: FC = async () => {
   const dictionary = Dictate.en
 
-  const blogs = await Drawer.getBlogSuggestions()
+  const { data: blogs } = await Drawer.getBlogSuggestions()
 
   const blueprint = new BluePrint(dictionary).owner()
 
@@ -20,9 +21,18 @@ const MainPage: FC = async () => {
       {dictionary.pages.main.description}
     </p>
 
-    <pre>
-      {JSON.stringify(blogs, null, 2)}
-    </pre>
+    <section>
+      <h2 className="mb-4">
+        {dictionary.pages.main.suggestedBlogPosts}
+      </h2>
+
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {blogs.map(blog => <BlogSuggestionCard
+          key={blog.slug}
+          {...blog}
+        />)}
+      </div>
+    </section>
   </Content>
 }
 
---
+++ src/components/pages/main/BlogSuggestionCard.tsx
@@ -0,0 +1,28 @@
+import Picture from "@/utilities/Picture"
+import Image from "next/image"
+import Link from "next/link"
+import { FC } from "react"
+
+const BlogSuggestionCard: FC<BlogListData> = (blog) => {
+  return <Link
+    href={`/blogs/${blog.slug}`}
+    className="bg-fedora-50 dark:bg-fedora-950 rounded-lg p-2 gap-2
+    no-underline text-fedora-950 dark:text-fedora-50 flex items-center
+    transition-all hover:shadow-md"
+  >
+    <Image
+      src={Picture.resolve(blog.cover)}
+      alt={blog.title}
+      width={114}
+      height={114}
+      className="rounded-md text-transparent m-0 object-cover aspect-square
+      shrink-0 w-16"
+    />
+
+    <h3 className="line-clamp-2 m-0 text-base">
+      {blog.title}
+    </h3>
+  </Link>
+}
+
+export default BlogSuggestionCard
