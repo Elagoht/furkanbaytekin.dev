b3ceb4f4b783faf27fb37f85ef4b32b8f739b056
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-16 23:13:54 +0300

    fix: update logging methods to include log level prefixes for better clarity

--- src/utilities/Printer/index.tsx
+++ src/utilities/Printer/index.tsx
@@ -1,18 +1,18 @@
 class Printer {
   public static log = (message: string): void => {
-    console.log(Printer.prefix(message, new Date()))
+    console.log(Printer.prefix("LOG", new Date()) + message)
   }
 
   public static warn = (message: string): void => {
-    console.warn(Printer.prefix(message, new Date()))
+    console.warn(Printer.prefix("WARN", new Date()) + message)
   }
 
   public static error = (message: string): void => {
-    console.error(Printer.prefix(message, new Date()))
+    console.error(Printer.prefix("ERROR", new Date()) + message)
   }
 
   public static info = (message: string): void => {
-    console.info(Printer.prefix(message, new Date()))
+    console.info(Printer.prefix("INFO", new Date()) + message)
   }
 
   private static prefix = (value: string, date: Date): string => {
