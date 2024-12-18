8e210d82fdccaf9deb73f77fb53f6a93dd63ffbe
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-18 23:55:44 +0300

    feat: enhance logging output with color formatting using chalk

--- src/utilities/Printer/index.tsx
+++ src/utilities/Printer/index.tsx
@@ -1,18 +1,20 @@
+import chalk from "chalk"
+
 class Printer {
   public static log = (message: string): void => {
-    console.log(Printer.prefix("LOG", new Date()) + message)
+    console.log(chalk.green(Printer.prefix("LOG", new Date())) + message)
   }
 
   public static warn = (message: string): void => {
-    console.warn(Printer.prefix("WARN", new Date()) + message)
+    console.warn(chalk.yellow(Printer.prefix("WARN", new Date())) + message)
   }
 
   public static error = (message: string): void => {
-    console.error(Printer.prefix("ERROR", new Date()) + message)
+    console.error(chalk.red(Printer.prefix("ERROR", new Date())) + message)
   }
 
   public static info = (message: string): void => {
-    console.info(Printer.prefix("INFO", new Date()) + message)
+    console.info(chalk.blue(Printer.prefix("INFO", new Date())) + message)
   }
 
   private static prefix = (value: string, date: Date): string => {
