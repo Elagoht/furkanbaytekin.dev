c3f18a5801f5df92411eb0c90b959b772fd6bf1d
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-12 15:32:57 +0300

    style: CORS headers configuration in Next.js setup

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,15 @@ import Picture from "@/utilities/Picture"
 import TrafficSign from "@/utilities/TrafficSign"
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return await Promise.resolve([
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "User-Agent, X-Webhook-Action, X-Webhook-Secret" },
-        ]
-      }
-    ])
-  },
+  headers: async () => Promise.resolve([{
+    source: "/api/:path*",
+    headers: [
+      { key: "Access-Control-Allow-Credentials", value: "true" },
+      { key: "Access-Control-Allow-Origin", value: "*" },
+      { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+      { key: "Access-Control-Allow-Headers", value: "User-Agent, X-Webhook-Action, X-Webhook-Secret" },
+    ]
+  }]),
   rewrites: TrafficSign.getRewrites,
   redirects: TrafficSign.getRedirects,
   images: { remotePatterns: Picture.getRemotePaths() }
